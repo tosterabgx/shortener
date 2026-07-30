f9158b385e110ec925899482cd9236d3e9cc0f42
tosterabgx <misiurenko.arsenii@gmail.com>
2026-07-31 00:20:53 +0200

    feat: implement MongoDB connection and links

---
+++ src/db.js
@@ -0,0 +1,38 @@
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  console.error("No env variable MONGODB_URI found");
+  process.exit(1);
+}
+const client = new MongoClient(process.env.MONGODB_URI);
+
+export const connectDB = async () => {
+  try {
+    await client.connect();
+    const collection = client.db().collection("links");
+    await collection.createIndex({ code: 1 }, { unique: true });
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+export const insertLink = async (data) => {
+  const collection = client.db().collection("links");
+  const doc = { ...data, createdAt: new Date() };
+  try {
+    await collection.insertOne(doc);
+    return doc;
+  } catch (err) {
+    if (err.code === 11000) return null;
+    throw err;
+  }
+};
+
+export const getLink = async (code) => {
+  const collection = client.db().collection("links");
+  const link = await collection.findOne({ code });
+  return link;
+};
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import path from "path";
-import { validateUrl } from "./utils.js";
 import { customAlphabet } from "nanoid";
+import path from "path";
+import { connectDB, insertLink, getLink } from "./db.js";
+import { validateUrl } from "./utils.js";
 
 const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const nanoid = customAlphabet(alphabet, 7);
@@ -12,22 +13,42 @@ const port = 3000;
 app.use(express.static(path.join(import.meta.dirname, "public")));
 app.use(express.json());
 
-app.post("/api/shorten", (req, res) => {
+app.post("/api/shorten", async (req, res) => {
   const { url } = req.body ?? {};
   if (!url) {
     return res.status(400).json({ message: "No URL provided" });
   }
 
-  const valid = validateUrl(url);
+  const valid = validateUrl(url)?.toString();
   if (!valid) {
     return res.status(400).json({ message: "Incorrect URL" });
   }
 
-  const code = nanoid(7);
+  let code, result;
+  let attempts = 0;
+
+  do {
+    if (++attempts > 5) {
+      return res.status(500).json({ message: "Could not generate code" });
+    }
+
+    code = nanoid(7);
+    result = await insertLink({ code, url: valid });
+  } while (!result);
 
   res.status(200).json({ short: "http://localhost:3000/" + code });
 });
 
+app.get("/:code", async (req, res) => {
+  const link = await getLink(req.params.code);
+  if (!link) {
+    return res.redirect("/");
+  }
+
+  res.redirect(301, link.url);
+});
+
+await connectDB();
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
